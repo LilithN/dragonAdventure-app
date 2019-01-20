e64033d2e889de1190469f43bbc03c7fe44e1ec9
Samantha Goodson <Shad0wech00@gmail.com>
2019-01-19 19:15:51 -0600

    new component

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -5,19 +5,18 @@ import { AgmCoreModule } from '@agm/core';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { MapScreenComponent } from './map-screen/map-screen.component';
-import { DragonsComponent } from './dragons/dragons.component';
 import { UserDashboardComponent } from './user-dashboard/user-dashboard.component';
+import { DragonInfoComponent } from './dragon-info/dragon-info.component';
 
 @NgModule({
   imports: [
     CommonModule,
     DashboardRoutingModule,
     HttpClientModule,
-    DragonsComponent,
     AgmCoreModule.forRoot({
-      apiKey: 'Inter your API Key Here'
+      apiKey: 'Enter your own API Key here'
     })
   ],
-  declarations: [MapScreenComponent, UserDashboardComponent]
+  declarations: [MapScreenComponent, UserDashboardComponent, DragonInfoComponent]
 })
 export class DashboardModule { }
---
+++ src/app/dashboard/dragon-info/dragon-info.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'da-dragon-info',
+  templateUrl: './dragon-info.component.html',
+  styleUrls: ['./dragon-info.component.css']
+})
+export class DragonInfoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
