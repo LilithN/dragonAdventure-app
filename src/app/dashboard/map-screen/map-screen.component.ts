4f229c1ac86af3ea4942443f0d73d2a63b72bef3
Samantha Goodson <Shad0wech00@gmail.com>
2019-02-06 14:01:28 -0600

    new click events
    got rid of unnescessary data and add two new click events for the two dragons

--- src/app/dashboard/map-screen/map-screen.component.ts
+++ src/app/dashboard/map-screen/map-screen.component.ts
@@ -1,73 +1,6 @@
 import { Component } from '@angular/core';
 import { MouseEvent, MarkerManager } from '@agm/core';
 
-// const randomNumber = Math.round( Math.random() * 2 );
-
-// Nadder JavaScript Data
-/* function nadder () {
-  const nadderNumber = Math.round( Math.random() * 2 );
-  document.getElementById('nadderTextBlock').style.display = 'inline-block';
-  document.getElementById('dramillionTextBlock').style.display = 'none';
-    document.getElementById('thunderdrumTextBlock').style.display = 'none';
-  if (nadderNumber === 0) {
-    document.getElementById('nadderText1').style.display = 'inline-block';
-    document.getElementById('nadderText2').style.display = 'none';
-    document.getElementById('nadderText3').style.display = 'none';
-  } else if (nadderNumber === 1) {
-    document.getElementById('nadderText1').style.display = 'none';
-    document.getElementById('nadderText2').style.display = 'inline-block';
-    document.getElementById('nadderText3').style.display = 'none';
-  } else {
-    document.getElementById('nadderText1').style.display = 'none';
-    document.getElementById('nadderText2').style.display = 'none';
-    document.getElementById('nadderText3').style.display = 'inline-block';
-  }
-  console.log (nadderNumber);
-} */
-
-// Dramillion JavaScript Data
-/* function dramillion () {
-  const dramillionNumber = Math.round( Math.random() * 2 );
-  document.getElementById('dramillionTextBlock').style.display = 'inline-block';
-  document.getElementById('nadderTextBlock').style.display = 'none';
-    document.getElementById('thunderdrumTextBlock').style.display = 'none';
-  if (dramillionNumber === 0) {
-    document.getElementById('dramillionText1').style.display = 'inline-block';
-    document.getElementById('dramillionText2').style.display = 'none';
-    document.getElementById('dramillionText3').style.display = 'none';
-  } else if (dramillionNumber === 1) {
-    document.getElementById('dramillionText1').style.display = 'none';
-    document.getElementById('dramillionText2').style.display = 'inline-block';
-    document.getElementById('dramillionText3').style.display = 'none';
-  } else {
-    document.getElementById('dramillionText1').style.display = 'none';
-    document.getElementById('dramillionText2').style.display = 'none';
-    document.getElementById('dramillionText3').style.display = 'inline-block';
-  }
-  console.log (dramillionNumber);
-  } */
-
-// Thunderdrum JavaScript Data
-/* function thunderdrum () {
-  const thunderdrumNumber = Math.round( Math.random() * 2 );
-  document.getElementById('thunderdrumTextBlock').style.display = 'inline-block';
-  document.getElementById('dramillionTextBlock').style.display = 'none';
-    document.getElementById('nadderTextBlock').style.display = 'none';
-  if (thunderdrumNumber === 0) {
-    document.getElementById('thunderdrumText1').style.display = 'inline-block';
-    document.getElementById('thunderdrumText2').style.display = 'none';
-    document.getElementById('thunderdrumText3').style.display = 'none';
-  } else if (thunderdrumNumber === 1) {
-    document.getElementById('thunderdrumText1').style.display = 'none';
-    document.getElementById('thunderdrumText2').style.display = 'inline-block';
-    document.getElementById('thunderdrumText3').style.display = 'none';
-  } else {
-    document.getElementById('thunderdrumText1').style.display = 'none';
-    document.getElementById('thunderdrumText2').style.display = 'none';
-    document.getElementById('thunderdrumText3').style.display = 'inline-block';
-  }
-  console.log (thunderdrumNumber);
-} */
 
 
 @Component({
@@ -78,29 +11,42 @@ import { MouseEvent, MarkerManager } from '@agm/core';
 
 export class MapScreenComponent {
 
-  markers: Marker[] = [
+  // Markers
+
+  nadderMarkers: Marker[] = [
     {
       lat: 22.128090,
       lng: -77.891641,
       label: 'Nadder',
       draggable: false,
       visible: true
-    }/* ,
-    {
-      lat: 22.124670,
-      lng: -77.980897,
-      label: 'Thunderdrum',
-      draggable: false
-    },
+    }
+  ];
+
+  dramillionMarkers: Marker[] = [
     {
       lat: 22.064235,
       lng: -77.860257,
       label: 'Dramillion',
-      draggable: false
-    } */
+      draggable: false,
+      visible: true
+    }
   ];
 
-  /* Map information */
+  thunderdrumMarkers: Marker[] = [
+    {
+      lat: 22.124670,
+      lng: -77.980897,
+      label: 'Thunderdrum',
+      draggable: false,
+      visible: true
+    }
+  ];
+
+
+
+
+  // Map information
 
   zoom = 11;
   lat = 22.104736;
@@ -112,50 +58,104 @@ export class MapScreenComponent {
   nadderZero = false;
   nadderOne = false;
   nadderTwo = false;
-  visibility = true;
+  dramillionStory = false;
+  dramillionZero = false;
+  dramillionOne = false;
+  dramillionTwo = false;
+  thunderdrumStory = false;
+  thunderdrumZero = false;
+  thunderdrumOne = false;
+  thunderdrumTwo = false;
+  nadderVisibility = true;
+  dramillionVisibility = true;
+  thunderdrumVisibility = true;
+  nInfo = false;
+  dInfo = false;
+  tInfo = false;
 
 
 
-
-  /*  clickedMarker(label: string, index: number) {
-     console.log(`clicked the marker: ${label || index}`);
-   } */
-
-  /* Dragon marker click functions */
-
-  /*   dragonClick(label: string) {
-      if (label === 'Nadder') {
-        console.log('clicked on Nadder');
-        nadder();
-      } else if (label === 'Thunderdrum') {
-        console.log('clicked on Thunderdrum');
-        thunderdrum();
-      } else if (label === 'Dramillion') {
-        console.log('clicked on Dramillion');
-        dramillion();
-      } else {
-        console.log('other');
-      }
-    } */
-
+  // Click Events
 
 
   nadderClick(label: string, visible: boolean, index: number) {
-    const nadderNumber = Math.round(Math.random() * 2);
+    const randomNumber = Math.round(Math.random() * 2);
     console.log(`clicked the marker: ${label || index}`);
-    if (nadderNumber === 1) {
-      console.log(nadderNumber);
+    this.nInfo = true;
+    this.dInfo = false;
+    this.tInfo = false;
+    if (randomNumber === 1) {
+      console.log(randomNumber);
       this.nadderStory = true;
       this.nadderOne = true;
-      // this.visibility = false;
-    } else if (nadderNumber === 2) {
-      console.log(nadderNumber);
+      this.nadderTwo = false;
+      this.nadderZero = false;
+    } else if (randomNumber === 2) {
+      console.log(randomNumber);
       this.nadderStory = true;
+      this.nadderOne = false;
       this.nadderTwo = true;
+      this.nadderZero = false;
     } else {
-      console.log(nadderNumber);
+      console.log(randomNumber);
       this.nadderStory = true;
+      this.nadderOne = false;
+      this.nadderTwo = false;
       this.nadderZero = true;
+    }
+  }
+
+  dramillionClick(label: string, visible: boolean, index: number) {
+    const randomNumber = Math.round(Math.random() * 2);
+    console.log(`clicked the marker: ${label || index}`);
+    this.nInfo = false;
+    this.dInfo = true;
+    this.tInfo = false;
+    if (randomNumber === 1) {
+      console.log(randomNumber);
+      this.dramillionStory = true;
+      this.dramillionOne = true;
+      this.dramillionTwo = false;
+      this.dramillionZero = false;
+    } else if (randomNumber === 2) {
+      console.log(randomNumber);
+      this.dramillionStory = true;
+      this.dramillionOne = false;
+      this.dramillionTwo = true;
+      this.dramillionZero = false;
+    } else {
+      console.log(randomNumber);
+      this.dramillionStory = true;
+      this.dramillionOne = false;
+      this.dramillionTwo = false;
+      this.dramillionZero = true;
+    }
+  }
+
+  thunderdrumClick(label: string, visible: boolean, index: number) {
+    const randomNumber = Math.round(Math.random() * 2);
+    console.log(`clicked the marker: ${label || index}`);
+    this.nInfo = false;
+    this.dInfo = false;
+    this.tInfo = true;
+    if (randomNumber === 1) {
+      console.log(randomNumber);
+      this.thunderdrumStory = true;
+      this.thunderdrumOne = true;
+      this.thunderdrumTwo = false;
+      this.thunderdrumZero = false;
+    } else if (randomNumber === 2) {
+      console.log(randomNumber);
+      this.thunderdrumStory = true;
+      this.thunderdrumOne = false;
+      this.thunderdrumTwo = true;
+      this.thunderdrumZero = false;
+    } else {
+      console.log(randomNumber);
+      this.thunderdrumStory = true;
+      this.thunderdrumOne = false;
+      this.thunderdrumTwo = false;
+      this.thunderdrumZero = true;
     }
   }
 
